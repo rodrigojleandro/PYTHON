d2c4d293ab205a9941cc644c6de63dd4be7d26f5
th3bl4ckcl0wn <32726780+th3bl4ckcl0wn@users.noreply.github.com>
2020-05-25 17:36:08 -0300

    Fazendo desafio 015

---
+++ Exercicios/EX015.py
@@ -0,0 +1,14 @@
+
+###############################################################################################################
+
+# DESAFIO: 015
+# TÍTULO: Aluguel de Carros
+# AULA: 07
+# EXERCÍCIO: Escreve um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade
+# de idas pelos quais ele foi alugado. Calcule o preço a pagar. Sabendo que o carro custa R$60,00 por dia e
+# R$0,15 por KM rodado.
+
+###############################################################################################################
+
+print("hello")
+
